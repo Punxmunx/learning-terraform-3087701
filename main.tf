a80ff1da1beed75890d130d86eb2bd23fa48e8e5
Punxmunx <38075219+Punxmunx@users.noreply.github.com>
2024-12-27 12:45:44 -0500

    updated module name ln 21

--- main.tf
+++ main.tf
@@ -18,17 +18,17 @@ data "aws_vpc" "default" {
   default = true 
 }
 
-module "vpc" {
+module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  azs              = ["us-east-1a", "us-east-1b", "us-east-1c"]
    public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
-    Terraform = "true"
+    Terraform   = "true"
     Environment = "dev"
   }
 }
