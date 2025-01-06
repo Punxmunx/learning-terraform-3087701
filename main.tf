90b4ab43032f78ebed28b3f02cd375e7523dce3e
Punxmunx <38075219+Punxmunx@users.noreply.github.com>
2025-01-06 13:08:22 -0500

    updated load balancer

--- main.tf
+++ main.tf
@@ -43,7 +43,37 @@ resource "aws_instance" "blog" {
     Name = "Learning Terraform"
   }
 }
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
 
+  name    = "blog-alb"
+  
+  load_balancer_type = "application"
+
+  vpc_id          = module.blog_vpc.vpc_id
+  subnets         = module.blog_vpc.public_subnets
+  security_groups = [module.blog_sg.security_group_id]
+  
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port = 80
+        }
+      }
+    }   
+  ]
+
+  tags = {
+    Environment = "Development"
+    Project     = "Example"
+  }
+}
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.2.0"
